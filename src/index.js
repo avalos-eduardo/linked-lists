a78c1599e15fb9b2bd53260685504d6dd62fea71
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-01-11 14:41:39 -0600

    adds to list and logs to test it

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Node } from "./node-class";
+import { LinkedList } from "./linked-list";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
